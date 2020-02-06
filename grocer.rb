077ccf56cec8db447d9b0e1b13a087bebbfe4efb
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-06 00:07:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ def find_item_by_name_in_collection(name, collection)
       return collection[n]
     end
     n += 1
-  end 
+  end
 end
 
 def consolidate_cart(cart)
@@ -16,6 +16,24 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  n_c = [] #new cart
+  n = 0
+  while n < cart.length do
+    n_c_i = find_item_by_name_in_collection(cart[n][:item], n_c) #new cart item
+    if n_c_i != nil
+      n_c_i[:count] += 1
+    else
+      n_c_i = {
+        :item => cart[n][:item],
+        :price => cart[n][:price],
+        :clearance => cart[n][:clearance],
+        :count => 1
+      }
+      n_c.append(n_c_i) #To add items to the new cart.
+    end
+    n += 1
+  end
+  return n_c 
 end
 
 def apply_coupons(cart, coupons)
